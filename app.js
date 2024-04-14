31f5a195c061d27d1b456a1f54e7478a505edae4
synuo <127924043+synuo@users.noreply.github.com>
2024-04-14 18:28:50 +0900

    fix/app.js app.use(cors())에 origin:* 추

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
 }));
-app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use('/signup', signupRouter);
 app.use('/addpost', addPostRouter);
