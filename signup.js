6a6468a37e8839721a0a58344008b9f3f36167b9
anchaewon <anchaewon327@naver.com>
2024-04-28 20:35:33 +0900

    Update signup.js

--- signup.js
+++ signup.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 app.post('/', (req, res) => {
-    const { email, password, name, student_id, nickname, user_image, major1, major1_change_log, major2, major3, introduction, all_noti, chatroom_noti, qna_noti, accept_noti, review_noti } = req.body;
+    const { email, password, name, student_id, nickname, user_image, major1, major1_change_log, major2, major3, introduction } = req.body;
 
     const newUser = {
         email,
@@ -20,11 +20,6 @@ app.post('/', (req, res) => {
         major2: major2 || null,
         major3: major3 || null,
         introduction: introduction || null,
-        all_noti,
-        chatroom_noti: all_noti ? chatroom_noti : false,
-        qna_noti: all_noti ? qna_noti : false,
-        accept_noti: all_noti ? accept_noti : false,
-        review_noti: all_noti ? review_noti : false
     };
 
     db.query('INSERT INTO users SET ?', newUser, (err, results) => {
