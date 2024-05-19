f93fc35f26da8374a776ab41da8f2bc8d87ac458
synuo <127924043+synuo@users.noreply.github.com>
2024-05-20 05:48:54 +0900

    Update login.js

--- login.js
+++ login.js
@@ -17,8 +17,15 @@ router.post('/', function(req, res) {
             if (results.length > 0) {
                 req.session.loggedin = true;
                 req.session.email = email;
+                // 로그인 성공 시 해당 유저의 아이디 반환
+                var userId = results[0].user_id;  // 결과에서 user_id를 가져옴
+                console.log(userId);
+
                 // 로그인 성공 시 JSON 응답 반환
-                res.status(200).json({ message: "Logged in successfully" });
+                res.status(200).json({
+                     message: "Logged in successfully",
+                     userId: userId // userId를 응답에 포함 
+                    });
             } else {
                 // 로그인 실패 시 JSON 응답 반환
                 res.status(401).json({ message: "Invalid email or password" });
