fcdc4b910f6474503989bc274cbf581b4b0eafe4
synuo <127924043+synuo@users.noreply.github.com>
2024-05-26 15:57:36 +0900

    UserId -> user_id 변수 통일

--- login.js
+++ login.js
@@ -18,13 +18,13 @@ router.post('/', function(req, res) {
                 req.session.loggedin = true;
                 req.session.email = email;
                 // 로그인 성공 시 해당 유저의 아이디 반환
-                var userId = results[0].user_id;  // 결과에서 user_id를 가져옴
-                console.log(userId);
+                var user_id = results[0].user_id;  // 결과에서 user_id를 가져옴
+                console.log(user_id);
 
                 // 로그인 성공 시 JSON 응답 반환
                 res.status(200).json({
                      message: "Logged in successfully",
-                     userId: userId // userId를 응답에 포함 
+                     user_id: user_id // userId를 응답에 포함 
                     });
             } else {
                 // 로그인 실패 시 JSON 응답 반환
