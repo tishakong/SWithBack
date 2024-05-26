2f66c73723a0c3f8d20e1f6763880a97c3ff44fd
anchaewon <anchaewon327@naver.com>
2024-05-26 22:40:10 +0900

    Update getreview.js

--- getreview.js
+++ getreview.js
@@ -2,26 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('./db.js');
 
-router.get('/', (req, res) => {
-  const {reviewee_id} = req.query;
+router.get('/user/:userId/reviews', (req, res) => {
+    const userId = req.params.userId;
+    const sql = `SELECT * FROM reviews WHERE reviewee_id = ?`;
 
-  const sql = `
-    SELECT * FROM reviews WHERE (reviewee_id=?)
-  `;
-  const values = [reviewee_id];
-
-  db.query(sql, values, (err, results) => {
-    if (err) {
-      console.error('Error querying database: ' + err.stack);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    if (results.length === 0){
-      res.status(202).send('작성된 리뷰가 없습니다.');
-      return;
-    }
-    res.status(201).json(results);
-  });
+    db.query(sql, [userId], (err, results) => {
+        if (err) {
+            console.error('Error querying database: ' + err.stack);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        if (results.length === 0){
+            res.status(202).send('작성된 리뷰가 없습니다.');
+            return;
+        }
+        res.status(200).json(results);
+    });
 });
 
 module.exports = router;
