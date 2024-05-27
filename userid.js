3fb29979be7ed4d3ae168383b67a77c23ac3755e
synuo <127924043+synuo@users.noreply.github.com>
2024-05-28 01:46:39 +0900

    fix/사용자 정보 수정 시 업뎃

--- userid.js
+++ userid.js
@@ -25,4 +25,54 @@ app.get('/:user_id', (req, res) => {
     });
 });
 
+//(소윤 추가)
+// 유저 정보 업데이트
+app.put('/:user_id', (req, res) => {
+    const user_id = req.params.user_id;
+    const { nickname, major1, major2, major3, major1_change_log, introduction, all_noti, chatroom_noti, qna_noti, accept_noti, review_noti } = req.body;
+
+    const query = `
+        UPDATE users 
+        SET 
+            nickname = ?, 
+            major1 = ?, 
+            major2 = ?, 
+            major3 = ?, 
+            major1_change_log = ?, 
+            introduction = ?, 
+            all_noti = ?, 
+            chatroom_noti = ?, 
+            qna_noti = ?, 
+            accept_noti = ?, 
+            review_noti = ?
+        WHERE user_id = ?
+    `;
+
+    const values = [nickname, major1, major2, major3, major1_change_log, introduction, all_noti, chatroom_noti, qna_noti, accept_noti, review_noti, user_id];
+
+    db.query(query, values, (err, result) => {
+        if (err) {
+            console.error('Error updating user information: ' + err.stack);
+            res.status(500).json({ error: 'Internal server error' });
+            return;
+        }
+
+        if (result.affectedRows === 0) {
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
+
+        res.status(200).json({ message: 'User updated successfully' });
+    });
+});
+
+
+//
+//app.listen(port, () => {
+  //  console.log(`Server is running on port ${port}`);
+//});
+
+
+//
+
 module.exports = app;
