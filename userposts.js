c26695296c4861353e024fe2ced629a40b1ba3c4
anchaewon <anchaewon327@naver.com>
2024-05-28 04:03:10 +0900

    Update userposts.js

--- userposts.js
+++ userposts.js
@@ -2,17 +2,21 @@ const express = require('express');
 const router = express.Router();
 const db = require('./db.js');
 
-router.get('/user/:userId/posts', (req, res) => {
-    const userId = req.params.userId;
-    const sql = `SELECT * FROM posts WHERE writer_id = ?`;
+router.get('/posts/:postId', (req, res) => {
+    const postId = req.params.postId;
+    const sql = `SELECT * FROM posts WHERE post_id = ?`;
 
-    db.query(sql, [userId], (err, results) => {
+    db.query(sql, [postId], (err, results) => {
         if (err) {
             console.error('Error querying database: ' + err.stack);
             res.status(500).send('Internal Server Error');
             return;
         }
-        res.status(200).json(results);
+        if (results.length > 0) {
+            res.status(200).json(results[0]); // 단일 객체 반환
+        } else {
+            res.status(404).send('Post not found');
+        }
     });
 });
 
